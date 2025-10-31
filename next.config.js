5c65d6a3ff984b225d7aa17246fbb198fc0cd65b
sagar sangwan <iamsagarsangwan@gmail.com>
2025-10-31 19:46:17 +0530

    added next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ... other config options
+
+  // ➡️ ADD THIS CONFIGURATION
+  experimental: {
+    // This setting tells Next.js to include all files in the
+    // .prisma/client directory in the final Vercel build output
+    outputFileTracingIncludes: {
+      "/": ["./node_modules/.prisma/client/**/*"],
+    },
+  },
+};
+
+module.exports = nextConfig;
